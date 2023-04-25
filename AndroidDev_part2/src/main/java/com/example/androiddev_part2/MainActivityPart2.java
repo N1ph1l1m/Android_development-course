4462f77d7d928bdd3ac5caa3d666489b005f220a
N1ph1l1m <zerorad@inbox.ru>
2023-04-25 13:07:39 +0300

    Module 2 - Commite 2.0

---
+++ AndroidDev_part2/src/main/java/com/example/androiddev_part2/MainActivityPart2.java
@@ -0,0 +1,14 @@
+package com.example.androiddev_part2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityPart2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_part2);
+    }
+}
