911b70de8283f70056789a95d52c8db71a65a5ca
N1ph1l1m <zerorad@inbox.ru>
2023-04-26 10:22:37 +0300

    Module 2 - Commite 2.2 - Event onClick

--- AndroidDev_part2/src/main/java/com/example/androiddev_part2/MainActivityPart2.java
+++ AndroidDev_part2/src/main/java/com/example/androiddev_part2/MainActivityPart2.java
@@ -3,6 +3,8 @@ package com.example.androiddev_part2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivityPart2 extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivityPart2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_part2);
+    }
+
+    int quantity = 0;
+
+    public void minusQuantity(View view) {
+        TextView quantityTextView = findViewById(R.id.num_quantity);
+        if(quantity>0){
+            quantity=quantity-1;
+            quantityTextView.setText(" " + quantity);
+        }
+
+    }
+
+
+    public void plusQuantity(View view) {
+        TextView quantityTextView = findViewById(R.id.num_quantity);
+        quantity=quantity+1;
+        quantityTextView.setText(" "+ quantity);
+
+
+
     }
 }
