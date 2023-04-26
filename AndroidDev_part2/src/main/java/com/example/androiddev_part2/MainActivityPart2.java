a739440197da7f3688ec0d0a60484fb93ae73cc1
N1ph1l1m <zerorad@inbox.ru>
2023-04-26 11:29:55 +0300

    Module 2 - Commite 2.3 - Event onClick,add price operations

--- AndroidDev_part2/src/main/java/com/example/androiddev_part2/MainActivityPart2.java
+++ AndroidDev_part2/src/main/java/com/example/androiddev_part2/MainActivityPart2.java
@@ -2,11 +2,12 @@ package com.example.androiddev_part2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 public class MainActivityPart2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,22 +15,34 @@ public class MainActivityPart2 extends AppCompatActivity {
         setContentView(R.layout.activity_main_part2);
     }
 
-    int quantity = 0;
 
+    int quantity = 0;
+    int price = 0;
+
+
+    @SuppressLint("SetTextI18n")
     public void minusQuantity(View view) {
         TextView quantityTextView = findViewById(R.id.num_quantity);
+        TextView Price = findViewById(R.id.Price);
         if(quantity>0){
-            quantity=quantity-1;
+            quantity-=1;
+            price-=200;
             quantityTextView.setText(" " + quantity);
+            Price.setText(" "+ price);
         }
 
     }
 
 
+    @SuppressLint("SetTextI18n")
     public void plusQuantity(View view) {
         TextView quantityTextView = findViewById(R.id.num_quantity);
-        quantity=quantity+1;
+        TextView Price = findViewById(R.id.Price);
+        quantity+=1;
+        price+=200;
         quantityTextView.setText(" "+ quantity);
+        Price.setText(" "+ price);
+
 
 
 
