c70e7108a63964a46d4ba77b473599ce81c9c514
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-04-30 21:53:58 +0300

    Module 3 - Commite 3.0 -  Create module 3

---
+++ AndroidDev_part3/src/main/java/com/example/androiddev_part3/AndroidDev_part3.java
@@ -0,0 +1,14 @@
+package com.example.androiddev_part3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AndroidDev_part3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_dev_part3);
+    }
+}
