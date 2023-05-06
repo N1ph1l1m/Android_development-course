e787ee7315b59d32621aba22899d13c54f87f1b1
N1ph1l1m <zerorad@inbox.ru>
2023-05-06 13:54:39 +0300

    Module 3 - Commite 3.6 - Add video in activity - Video (does not work)

--- AndroidDev_part3/src/main/java/com/example/androiddev_part3/Video.java
+++ AndroidDev_part3/src/main/java/com/example/androiddev_part3/Video.java
@@ -3,6 +3,8 @@ package com.example.androiddev_part3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Video extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demo);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
