0da46e179d4dcaa85aaf2230076255b4a0f93872
N1ph1l1m <zerorad@inbox.ru>
2023-05-06 13:35:55 +0300

    Module 3 - Commite 3.6 - Work  activity - Audio

--- AndroidDev_part3/src/main/java/com/example/androiddev_part3/Audio.java
+++ AndroidDev_part3/src/main/java/com/example/androiddev_part3/Audio.java
@@ -2,13 +2,28 @@ package com.example.androiddev_part3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Audio extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.stuff2);
+    }
+    boolean play_audio = true;
+    public void clickStart(View view) {
+        mediaPlayer.start();
+    }
+
+    public void clickPause(View view) {
+        mediaPlayer.pause();
     }
 }
