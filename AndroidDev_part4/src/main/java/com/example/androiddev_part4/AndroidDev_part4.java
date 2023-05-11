bc9ff45ddd4f21b07a8fa6b5e1e4f5e70a7bf43a
N1ph1l1m <zerorad@inbox.ru>
2023-05-11 15:51:32 +0300

    Module 4 - Commite 4.1 - Create ListView

--- AndroidDev_part4/src/main/java/com/example/androiddev_part4/AndroidDev_part4.java
+++ AndroidDev_part4/src/main/java/com/example/androiddev_part4/AndroidDev_part4.java
@@ -3,6 +3,12 @@ package com.example.androiddev_part4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class AndroidDev_part4 extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class AndroidDev_part4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_dev_part4);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i<20; i++){
+            list.add("Post" + i);
+            Log.i("list" , list.get(i));
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter((arrayAdapter));
     }
+
 }
