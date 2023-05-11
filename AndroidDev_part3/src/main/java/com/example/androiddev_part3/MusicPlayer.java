7bc46956eb6ba76e62fe2189a6af5c2811d383d2
N1ph1l1m <zerorad@inbox.ru>
2023-05-11 12:13:41 +0300

    Module 3 - Commite 3.14 - Add desighn and logic button  - MusicPlayer

--- AndroidDev_part3/src/main/java/com/example/androiddev_part3/MusicPlayer.java
+++ AndroidDev_part3/src/main/java/com/example/androiddev_part3/MusicPlayer.java
@@ -8,15 +8,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MusicPlayer extends AppCompatActivity {
 
 
     MediaPlayer mediaPlayer;
+    ImageView pauseIcon;
     Button buttonPlay;
+    ImageView imageViewPlay;
     AudioManager audioManager;
     SeekBar volumeSeekBar;
+    SeekBar musicSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +32,38 @@ public class MusicPlayer extends AppCompatActivity {
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
-        buttonPlay = findViewById(R.id.buttonPlay);
+        pauseIcon = findViewById(R.id.viewPlay);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.stuff2);
+
+        musicSeekBar = findViewById(R.id.seekbarMusic);
+        musicSeekBar.setMax(mediaPlayer.getDuration());
+        musicSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                musicSeekBar.setProgress (mediaPlayer.getCurrentPosition());
+            }
+        },0,1000);
         volumeSeekBar = findViewById(R.id.seekbarVolume);
         volumeSeekBar.setMax(maxVolume);
-
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -42,27 +77,35 @@ public class MusicPlayer extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
-        buttonPlay.setOnClickListener( new View.OnClickListener(){
-            public void onClick(View v) {
-                if(mediaPlayer.isPlaying()){
-                    pause();
-                }else{
-                    start();
-                }
-            }
-        });
-
-        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.stuff2);
-
-    }
-    public void start() {
-        mediaPlayer.start();
-        buttonPlay.setText("Pause");
     }
 
-    public void pause() {
+
+    public void musicPlay(View view) {
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            pauseIcon.setImageResource(R.drawable.play_arrow);
+        }else{
+            mediaPlayer.start();
+
+            pauseIcon.setImageResource(R.drawable.pause_music);
+        }
+    }
+
+    public void endMusic(View view){
+
+        musicSeekBar.setProgress(mediaPlayer.getDuration());
+        mediaPlayer.seekTo(mediaPlayer.getDuration());
         mediaPlayer.pause();
-        buttonPlay.setText("Play");
+        pauseIcon.setImageResource(R.drawable.play_arrow);
+
+
+    }
+    public void startMusic(View view){
+        musicSeekBar.setProgress(0);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.pause();
+        pauseIcon.setImageResource(R.drawable.play_arrow);
+
     }
 
 }
