c3eb80c8366f9549070fdab776a4f44dc04d165f
N1ph1l1m <zerorad@inbox.ru>
2023-06-05 15:01:21 +0300

    Module 6 - Commit 6.0 - Create module 6

---
+++ AndroidDev_part6/src/main/java/com/example/androiddev_part6/MainActivity_module_6.java
@@ -0,0 +1,14 @@
+package com.example.androiddev_part6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_module_6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_module6);
+    }
+}
