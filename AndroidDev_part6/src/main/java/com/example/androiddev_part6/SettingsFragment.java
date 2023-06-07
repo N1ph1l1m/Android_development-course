245ff0e62e58906a29a116fc604fb6361933c729
N1ph1l1m <zerorad@inbox.ru>
2023-06-07 13:55:42 +0300

    Module 6 - Commit 6.15 - Add files for project

---
+++ AndroidDev_part6/src/main/java/com/example/androiddev_part6/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.example.androiddev_part6;
+
+
+//import android.os.Bundle;
+//import android.support.v7.preference.PreferenceFragmentCompat;
+
+import android.os.Bundle;
+
+public class SettingsFragment  {
+//    @Override
+//    public void onCreatePreferences(Bundle bundle, String s) {
+//        addPreferencesFromResource(R.xml.timer_preferences);
+//
+//    }
+}
--- AndroidDev_part6/src/main/java/com/example/androiddev_part6/action_activity.java
+++ AndroidDev_part6/src/main/java/com/example/androiddev_part6/action_activity.java
@@ -1,8 +1,12 @@
 package com.example.androiddev_part6;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class action_activity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class action_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
+
+        ActionBar actionBar = this.getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
