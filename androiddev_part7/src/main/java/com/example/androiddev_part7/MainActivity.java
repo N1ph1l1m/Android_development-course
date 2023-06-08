25d9e69665dbefa816c70072e4f010060efb7cbd
N1ph1l1m <zerorad@inbox.ru>
2023-06-08 11:40:13 +0300

    Create module 7  - Application  name Car_DB

---
+++ androiddev_part7/src/main/java/com/example/androiddev_part7/MainActivity.java
@@ -0,0 +1,14 @@
+  package com.example.androiddev_part7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
