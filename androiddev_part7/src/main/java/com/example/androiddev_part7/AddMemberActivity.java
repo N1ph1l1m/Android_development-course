c8cd9080fc72dfec88a452bc39b2f4089665bfef
N1ph1l1m <zerorad@inbox.ru>
2023-06-12 16:44:16 +0300

    Module 7 - Commit 7.2 - Add array adapter and array.xml file

--- androiddev_part7/src/main/java/com/example/androiddev_part7/AddMemberActivity.java
+++ androiddev_part7/src/main/java/com/example/androiddev_part7/AddMemberActivity.java
@@ -3,8 +3,11 @@ package com.example.androiddev_part7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AddMemberActivity extends AppCompatActivity {
 
@@ -12,7 +15,9 @@ public class AddMemberActivity extends AppCompatActivity {
     private EditText lastNameEditText;
     private EditText groupNameEditText;
     private Spinner genderSpinner;
-    private int gender = 0;
+    private int     gender = 0;
+    private ArrayAdapter spinnerAdapter;
+
 
 
     @Override
@@ -24,6 +29,14 @@ public class AddMemberActivity extends AppCompatActivity {
         lastNameEditText = findViewById(R.id.editLastName);
         genderSpinner = findViewById(R.id.spinnerGender);
         groupNameEditText = findViewById(R.id.groupNameEdit);
+
+
+
+        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(spinnerAdapter);
+
+
 
 
 
