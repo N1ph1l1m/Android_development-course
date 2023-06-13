e94e9513f185964c7d478eae3acd2177aac29f0c
N1ph1l1m <zerorad@inbox.ru>
2023-06-13 15:47:41 +0300

    Module 7 - Commit 7.11 - Update files : MainActivity.java

--- androiddev_part7/src/main/java/com/example/androiddev_part7/MainActivity.java
+++ androiddev_part7/src/main/java/com/example/androiddev_part7/MainActivity.java
@@ -3,17 +3,24 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.androiddev_part7.data.ClubOlympContract;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
   public class MainActivity extends AppCompatActivity {
 
+      TextView dataTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataTextView = findViewById(R.id.dataTextWiew);
+
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +33,53 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
         });
 
 
+    }
+    protected  void onStart(){
+        super.onStart();
+        displayData();
+    }
+    private  void displayData() {
+        String[] projection = {
+                ClubOlympContract.MemberEntry._ID,
+                ClubOlympContract.MemberEntry.KEY_FIRST_NAME,
+                ClubOlympContract.MemberEntry.KEY_LAST_NAME,
+                ClubOlympContract.MemberEntry.KEY_GENDER,
+                ClubOlympContract.MemberEntry.KEY_SPORT
+        };
+
+        Cursor cursor = getContentResolver().query(ClubOlympContract.MemberEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
+        dataTextView.setText("All member \n\n");
+        dataTextView.append(ClubOlympContract.MemberEntry._ID + " " +
+                ClubOlympContract.MemberEntry.KEY_FIRST_NAME + " " +
+                ClubOlympContract.MemberEntry.KEY_LAST_NAME + " " +
+                ClubOlympContract.MemberEntry.KEY_GENDER + " " +
+                ClubOlympContract.MemberEntry.KEY_SPORT);
+
+        int idIndex = cursor.getColumnIndex(ClubOlympContract.MemberEntry._ID);
+        int  idFirstName= cursor.getColumnIndex(ClubOlympContract.MemberEntry.KEY_FIRST_NAME);
+        int idLastName = cursor.getColumnIndex(ClubOlympContract.MemberEntry.KEY_LAST_NAME);
+        int idGender = cursor.getColumnIndex(ClubOlympContract.MemberEntry.KEY_GENDER);
+        int idSport = cursor.getColumnIndex(ClubOlympContract.MemberEntry.KEY_SPORT);
+
+        while (cursor.moveToNext()){
+            int currentId = cursor.getInt(idIndex);
+            String currentFirstName = cursor.getString(idFirstName);
+            String currentLastName = cursor.getString(idLastName);
+            int currentGender = cursor.getInt(idGender);
+            String currentSport = cursor.getString(idSport);
+
+            dataTextView.append("\n" +
+                    currentId + " "+
+                    currentFirstName + " " +
+                    currentLastName + " " +
+                    currentGender + " " +
+                    currentSport  +  " " );
+        }
+        cursor.close();
     }
 }
